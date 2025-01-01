d3e92e9915088c97631c6e7dd8438980d09fc3d5
Aniket Das <anisoccer749@gmail.com>
2025-01-01 23:11:02 +0530

    test

--- BrightLayers/src/pages/Home.jsx
+++ BrightLayers/src/pages/Home.jsx
@@ -38,41 +38,41 @@ function Home() {
   return (
     <div className='w-auto h-auto bg-[#ecf0f1] overflow-hidden z-[-1] mt-[10%]'>
       <Navbar />
-            <div className='w-auto flex items-center justify-center h-[500px] BrightLayers'>
-              <div className='w-[1215px] text-center'>
-                  <span className='text-[#303338] text-[55px] font-medium, font-pSans leading-[64.90px] tracking-wide'>
+            <div className='w-auto flex items-center justify-center h-[31.25rem] BrightLayers'>
+              <div className='w-[75.9375rem] text-center'>
+                  <span className='text-[#303338] text-[3.4375rem] font-medium, font-pSans leading-[4.0563rem] tracking-wide'>
                       We are Bright Layers, a dynamic social media agency since 2019,
                       dedicated to elevating brands through creative content and strategic
                       digital marketing
                     </span>
-                    <span className="text-[#303338] text-[66px] font-medium font-pSans leading-[77.88px] tracking-wide">
+                    <span className="text-[#303338] text-[4.125rem] font-medium font-pSans leading-[4.8675rem] tracking-wide">
                         {" "}
                       </span>
-                    <span className="text-[#303338] text-[49px] font-medium font-pSans leading-[57.82px] tracking-wide">
+                    <span className="text-[#303338] text-[3.0625rem] font-medium font-pSans leading-[3.6138rem] tracking-wide">
                           ツ
                       </span>
                </div>
             </div>
-            <div className="w-full h-[222px] flex justify-end mt-36" id='about'>
+            <div className="w-full h-[13.875rem] flex justify-end mt-36" id='about'>
                 <img
                     src={home1}
-                    className="w-[1200px] h-[222px] object-cover"
+                    className="w-[75rem] h-[13.875rem] object-cover"
                     alt="assets"
                   />
               </div>
 
 
-            <div className="w-auto h-[324px] flex flex-col mt-36 px-4">
+            <div className="w-auto h-[20.25rem] flex flex-col mt-36 px-4">
                     <div
-                      className="w-[495px] h-[19px] text-right text-[#da5428] 
-                      text-[25px] font-semibold font-pSans
-                      capitalize leading-[29.5px] tracking-tight">
+                      className="w-[30.9375rem] h-[1.1875rem] text-right text-[#da5428] 
+                      text-[1.5625rem] font-semibold font-pSans
+                      capitalize leading-[1.8438rem] tracking-tight">
                       What Do We Do?
                       </div>
 
                 <div
-                    className="w-[863px] h-[364px] text-[#535353] 
-                    text-[24.5px] font-medium font-title leading-9 tracking-tight text-left mt-4 mx-auto">
+                    className="w-[53.9375rem] h-[22.75rem] text-[#535353] 
+                    text-[1.5313rem] font-medium font-title leading-9 tracking-tight text-left mt-4 mx-auto">
                     In the last 5 years, the country has seen trendsetting marketing
                     campaigns — but what&apos;s missing is &quot;the beyonds&quot; of a campaign. Does it
                     end after a post, a hoarding, an ad? So we just want likes & comments?
@@ -85,31 +85,31 @@ function Home() {
                     works exclusively for you.
                   </div>
                 <div className='flex justify-end'>
-                    <a href='#examples' className="w-[151px] h-[29px] text-[#535353] text-xl font-semibold font-title underline leading-relaxed tracking-tight mb-4">
+                    <a href='#examples' className="w-[9.4375rem] h-[1.8125rem] text-[#535353] text-xl font-semibold font-title underline leading-relaxed tracking-tight mb-4">
                         check examples
                     </a>
                   </div>
                 </div>
-                <div className='w-full h-[222px] flex justify-start mt-40'>
-                    <img src={image} alt="assets"  className='w-[1200px] h-[222px] object-cover'/>
+                <div className='w-full h-[13.875rem] flex justify-start mt-40'>
+                    <img src={image} alt="assets"  className='w-[75rem] h-[13.875rem] object-cover'/>
                   </div>
-                    <div className='w-auto h-[324px] flex flex-col mt-36 px-4'>
-                        <div className='w-[509px] h-[19px] text-right text-[#da5428] 
-                          text-[25px] font-semibold font-pSans
-                          capitalize leading-[29.5px] tracking-tight'>How Do We Do It?</div>
-                            <div className='w-[863px] h-[364px] text-[#535353] 
-                                text-[24.5px] font-medium font-title leading-9 tracking-tight text-left mt-4 mx-auto'>Our approach is different - a boutique-style agency that prioritizes understanding our   clients as intimately as our own business. This approach has not only made us experts in our field but has allowed us to build genuine partnerships.</div>
+                    <div className='w-auto h-[20.25rem] flex flex-col mt-36 px-4'>
+                        <div className='w-[31.8125rem] h-[1.1875rem] text-right text-[#da5428] 
+                          text-[1.5625rem] font-semibold font-pSans
+                          capitalize leading-[1.8438rem] tracking-tight'>How Do We Do It?</div>
+                            <div className='w-[53.9375rem] h-[22.75rem] text-[#535353] 
+                                text-[1.5313rem] font-medium font-title leading-9 tracking-tight text-left mt-4 mx-auto'>Our approach is different - a boutique-style agency that prioritizes understanding our   clients as intimately as our own business. This approach has not only made us experts in our field but has allowed us to build genuine partnerships.</div>
                                 <div className='flex justify-end'>
-                                      <a href='#examples' className="w-[151px] h-[156px] text-[#535353] text-xl font-semibold font-title underline leading-relaxed tracking-tight mb-4" id='service'>
+                                      <a href='#examples' className="w-[9.4375rem] h-[9.75rem] text-[#535353] text-xl font-semibold font-title underline leading-relaxed tracking-tight mb-4" id='service'>
                                           check services
                                         </a>
                                     </div>
                                 </div>
-                          <div className='w-[550px] h-[137px] text-[#303338] text-[45px] font-semibold font-pSans leading-4 tracking-tight mt-10 ml-[146px]'>Service We Provide For Your Brand
-                                <span className="text-[#303338] text-[66px] font-medium font-pSans leading-[77.88px] tracking-wide">
+                          <div className='w-[34.375rem] h-[8.5625rem] text-[#303338] text-[2.8125rem] font-semibold font-pSans leading-4 tracking-tight mt-10 ml-[9.125rem]'>Service We Provide For Your Brand
+                                <span className="text-[#303338] text-[4.125rem] font-medium font-pSans leading-[4.8675rem] tracking-wide">
                                     {" "}
                                   </span>
-                                <span className="text-[#303338] text-[47px] font-bold font-pSans leading-[57.82px] tracking-wide">
+                                <span className="text-[#303338] text-[2.9375rem] font-bold font-pSans leading-[3.6138rem] tracking-wide">
                                     ツ
                                   </span>
                                     <AnimatedLogo />
@@ -117,7 +117,7 @@ function Home() {
                               <div className='flex items-center justify-between'>
                                   <DropDown />
                                   <div className='flex -my-24'>
-                                      <img src={rectangle} alt="image" className='mr-[80px]' />
+                                      <img src={rectangle} alt="image" className='mr-[5rem]' />
                                     </div>
                                 </div>
 
@@ -126,7 +126,7 @@ function Home() {
                                 </div>
 
                                   <div className='w-full flex justify-center'>  
-                                    <div className="w-[1139px] h-[313px] text-center text-[#535353] font-medium font-title leading-snug tracking-tight text-[25px]">
+                                    <div className="w-[71.1875rem] h-[19.5625rem] text-center text-[#535353] font-medium font-title leading-snug tracking-tight text-[1.5625rem]">
                     Our ideal customer personas are small to medium-sized business
                     owners, marketing managers, and ad moguls  looking to make their
                     brand be seen. They value the power of strategy with creativity, and
@@ -146,13 +146,13 @@ function Home() {
                 <KnowAboutUs />
             </div>
 
-            <div className="w-auto h-[36.30px] text-center text-[#303338] text-[37px] font-semibold font-pSans leading-[43.66px] tracking-tight mt-80">What People Are Saying</div>
+            <div className="w-auto h-[2.2687rem] text-center text-[#303338] text-[2.3125rem] font-semibold font-pSans leading-[2.7287rem] tracking-tight mt-80">What People Are Saying</div>
 
             <div className='flex justify-center'>
                 <div className='w-auto flex flex-row justify-between mt-[8.67rem] space-x-48'>
-                  <img src={image26} alt="eureka" className='w-[256px] h-[148px] ml-10'/>
-                  <img src={image25} alt="thames" className='w-[256px] h-[69px] mt-[2.5rem]'/>
-                  <img src={image27} alt="talash" className='w-[256px] h-[148px]'/>
+                  <img src={image26} alt="eureka" className='w-[16rem] h-[9.25rem] ml-10'/>
+                  <img src={image25} alt="thames" className='w-[16rem] h-[4.3125rem] mt-[2.5rem]'/>
+                  <img src={image27} alt="talash" className='w-[16rem] h-[9.25rem]'/>
                 </div>
               </div>
             <div>
